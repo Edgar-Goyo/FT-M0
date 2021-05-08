a8fdecd2bf079249cc2e0919ea517ba0d162ebea
Edgar-Goyo <edgarjavierdiaz@gmail.com>
2021-05-08 07:18:17 -0500

    04-JS-III/homework/ejercicios.js

---
+++ 04-JS-III/homework/ejercicios.js
@@ -0,0 +1,26 @@
+//function incrementarPorUno(array)
+//var a=[10,10,16,12]
+//for (let b=0; b<a.length; b++)
+//console.log (a[b]+1);
+
+//agregarItemAlFinalDelArray
+var palabras=["Henry", "JavaScriuptClass"]
+console.log (palabras.slice(0))+"";
+palabras.splice (1)
+console.log (palabras.slice(1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

a78cf990e32af7c88ec14a9c2ef7b6d3fc43fc94
Edgar-Goyo <edgarjavierdiaz@gmail.com>
2021-05-10 22:00:39 -0500

    05-JS-IV/homework/ejercicios.js

---
+++ 05-JS-IV/homework/ejercicios.js
@@ -0,0 +1,19 @@
+console.log ("Tarea5");
+//function crearGato(nombre, edad) {
+    // Crear un nuevo objeto con la propiedad "nombre" y el valor definido como el argumento "nombre".
+    // Agrega una propiedad al objeto con el nombre "edad" y usa el valor definido en el argumento "edad"
+    // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
+    // Devuelve el objeto
+    // Tu código:
+    
+
+    function  meow ("Meow");
+
+    { 
+
+        nombre = "nombre";
+        edad = edad;
+        
+
+    }
+  
